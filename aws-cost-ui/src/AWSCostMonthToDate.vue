7922ce5bc585e25231d6d15bbe80cf219bf2f818
Jędrzej Lewandowski <jedrzej@pitchup.com>
2019-12-28 23:46:05 +0000

    feat: add AWSCostMonthToDate to main ui

---
+++ aws-cost-ui/src/AWSCostMonthToDate.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="aws-cost-month-to-date">&nbsp;</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {},
+})
+export default class AWSCostMonthToDate extends Vue {}
+</script>
+
+<style>
+#aws-cost-month-to-date {
+  border: 1px solid red;
+}
+</style>
---
+++ aws-cost-ui/src/index.ts
@@ -0,0 +1,1 @@
+export { default as AWSCostMonthToDate } from './AWSCostMonthToDate.vue';
