ebb5dc4ce745b70013837f62c2e158ce1e9d8dec
Jędrzej Lewandowski <jedrzej@pitchup.com>
2020-01-05 23:24:39 +0000

    feat: setup firebase deployment

--- aws-cost-functions/src/handler.ts
+++ aws-cost-functions/src/handler.ts
@@ -5,7 +5,10 @@ import { AWSCostEntry } from '../../AWSCostEntry';
 import { getCredentials } from './getCredentials';
 import { getCostExplorer } from './getCostExplorer';
 
-export async function handler(firestore: FirebaseFirestore.Firestore, fetchFn: (costExplorer: AWS.CostExplorer) => Promise<AWSCostEntry>) {
+export async function handler(
+  firestore: FirebaseFirestore.Firestore,
+  fetchFn: (costExplorer: AWS.CostExplorer) => Promise<AWSCostEntry>,
+) {
   try {
     const costExplorer = await getCostExplorer(getCredentials());
     const costEntry: AWSCostEntry = await fetchFn(costExplorer);
@@ -19,7 +22,10 @@ export async function handler(firestore: FirebaseFirestore.Firestore, fetchFn: (
   }
 }
 
-async function saveCostEntry(firestore: FirebaseFirestore.Firestore, costEntry: Partial<AWSCostEntry>) {
+async function saveCostEntry(
+  firestore: FirebaseFirestore.Firestore,
+  costEntry: Partial<AWSCostEntry>,
+) {
   await firestore
     .collection(awsCostConfig.firestoreCollection)
     .doc(`${Date.now()}`)
