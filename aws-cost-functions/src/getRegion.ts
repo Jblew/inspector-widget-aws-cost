8c86283cb73e38e486d9fc981c724b7aaa02546b
Jędrzej Lewandowski <jedrzej@pitchup.com>
2020-01-05 23:18:23 +0000

    fix: make functions config key compliant with gcp rules

--- aws-cost-functions/src/getRegion.ts
+++ aws-cost-functions/src/getRegion.ts
@@ -2,8 +2,8 @@ import * as functions from 'firebase-functions';
 import ow from 'ow--fork-by-jblew-with-catching';
 
 export function getRegion() {
-  const region = functions.config().default_region;
-  ow(region, 'firebase.functions.config().default_region', ow.string.nonEmpty);
+  const region = functions.config().inspectorwidget.default_region;
+  ow(region, 'firebase.functions.config().inspectorwidget.default_region', ow.string.nonEmpty);
 
   return region;
 }
