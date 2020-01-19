a0af0ccaa684bbbb3106879f3180e59e6c729194
Jędrzej Lewandowski <jedrzej@pitchup.com>
2020-01-19 18:08:56 +0000

    feat: show data mock

--- ui/AWSCostMonthToDate.vue
+++ ui/AWSCostMonthToDate.vue
@@ -1,26 +1,32 @@
 <template>
   <div id="aws-cost-month-to-date">
     <h1>Aws cost</h1>
-    <stateful-resource :resource="costRes"> </stateful-resource>
+    <stateful-resource :resource="costResource">
+      <div v-for="(elem, index) in costResource.result" :key="index">
+        {{ JSON.stringify(elem) }}
+      </div>
+    </stateful-resource>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { StatefulResource, Resource } from 'vue-stateful-resource';
-import { getFirebaseSubcollection } from '@/helpers';
+import firebase from 'firebase/app';
 import { awsCostConfig } from '../aws-cost-config';
 import { AWSCostEntry } from '../AWSCostEntry';
 
-
 function getCol() {
-  return getFirebaseSubcollection(awsCostConfig.firestoreCollection);
+  console.log(`Col=${awsCostConfig.firestoreCollection}`);
+  return firebase.firestore().collection(awsCostConfig.firestoreCollection);
 }
 
 async function getData() {
   const qs = await getCol()
     .limit(100)
-    .orderBy('timestampMs', 'desc').get();
+    .orderBy('timestampMs', 'desc')
+    .where('type', '==', 'month_to_date')
+    .get();
   return qs.docs.map(doc => doc.data() as AWSCostEntry);
 }
 
@@ -28,13 +34,23 @@ async function getData() {
   components: { StatefulResource },
 })
 export default class AWSCostMonthToDate extends Vue {
-  costRes: Resource<AWSCostEntry[]> = Resource.empty()
+  costResource: Resource<AWSCostEntry[]> = Resource.empty();
+
+  get entries(): AWSCostEntry[] {
+    return this.costResource.result || [];
+  }
 
   beforeMount() {
-    Resource.fetchResource('aws-cost-month-to-date', () => getData(), (res) => { this.costRes = res; });
+    Resource.fetchResource(
+      'aws-cost-month-to-date',
+      () => getData(),
+      costResource => {
+        this.costResource = costResource;
+        console.log(costResource);
+      },
+    );
   }
 }
-
 </script>
 
 <style>
